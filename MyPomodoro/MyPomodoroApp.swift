7103896c3256673139aca39c68c5680e947cb822
Thiago Ramos <thiagoramos.al@gmail.com>
2020-08-09 16:57:20 -0300

    Initial Commit

---
+++ MyPomodoro/MyPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPomodoroApp.swift
+//  MyPomodoro
+//
+//  Created by Thiago Ramos on 09/08/20.
+//
+
+import SwiftUI
+
+@main
+struct MyPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
